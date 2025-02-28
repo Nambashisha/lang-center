ad7dac4813225782978e2950a131f2d5b5b987fe
Nambashisha Ryngksai <nambashisharyngksai@gmail.com>
2025-02-28 09:47:50 +0000

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,6 @@
+<iframe src="https://codesandbox.io/p/devbox/29h663?embed=1&file=%2Fsrc%2Fmain.js"
+     style="width:100%; height: 500px; border:0; border-radius: 4px; overflow:hidden;"
+     title="lang-center"
+     allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+     sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+   ></iframe>
